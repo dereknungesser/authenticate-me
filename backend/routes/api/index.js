f15347fc6f737a7a86cf935b052b0902e74d8e27
Derek Nungesser <nungesserfilms@gmail.com>
2020-12-17 13:08:01 -0500

    login routes complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+module.exports = router;
+
 // const asyncHandler = require('express-async-handler');
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
@@ -34,5 +43,3 @@ const router = require('express').Router();
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 // });
-
-module.exports = router;
