d2ebb8dc8f85f49db1759cab40092f656b32674a
Derek Nungesser <nungesserfilms@gmail.com>
2020-12-18 13:56:19 -0500

    completed csrf

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,15 @@ import App from './App';
 
 import configureStore from './store';
 
+import { restoreCSRF, fetch } from './store/csrf';
+
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
   window.store = store;
 }
 
